15de9d91a3ddcca254811c844abc5c933b39b4d4
Doug Service <doug@mathtix.com>
2024-06-23 17:39:37 -0500

    Initial uniform time series example

--- src/main/java/org/example/TimeSeries_AWT.java
+++ src/main/java/org/example/TimeSeries_AWT.java
@@ -1,6 +1,10 @@
+package org.example;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartUtils;
+
 import org.jfree.data.general.SeriesException;
 import org.jfree.data.time.Second;
 import org.jfree.data.time.TimeSeries;
@@ -9,14 +13,26 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class TimeSeries_AWT extends ApplicationFrame {
 
     public TimeSeries_AWT( final String title ) {
         super( title );
         final XYDataset dataset = createDataset( );
         final JFreeChart chart = createChart( dataset );
+
+        File file = new File( "./time-series.png");
+        try {
+            ChartUtils.saveChartAsPNG(file, chart, 1024,768);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         final ChartPanel chartPanel = new ChartPanel( chart );
-        chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 370 ) );
+        chartPanel.setPreferredSize( new Dimension( 1024 , 768 ) );
         chartPanel.setMouseZoomable( true , false );
         setContentPane( chartPanel );
     }
@@ -30,7 +46,7 @@ public class TimeSeries_AWT extends ApplicationFrame {
 
             try {
                 value = value + Math.random( ) - 0.5;
-                series.add(current, new Double( value ) );
+                series.add(current,  value );
                 current = ( Second ) current.next( );
             } catch ( SeriesException e ) {
                 System.err.println("Error adding to series");
@@ -58,4 +74,4 @@ public class TimeSeries_AWT extends ApplicationFrame {
         RefineryUtilities.positionFrameRandomly( demo );
         demo.setVisible( true );
     }
-}   
+}
